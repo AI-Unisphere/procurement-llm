1d3ae49001c6d63a44299006ff5dc05a3a3c33ef
LucasPilla <elucaspimentel@gmail.com>
2025-03-01 20:12:17 +0100

    added text extraction script

--- src/procurement_granite/data/dataset_text_extraction.py
+++ src/procurement_granite/data/dataset_text_extraction.py
@@ -1,47 +1,42 @@
 import json
 from pathlib import Path
 import os
-import requests
-import tempfile
-import pdfplumber
 import fitz  # PyMuPDF
 from docx import Document
-
+from tqdm import tqdm
 
 # Get the directory of the current script
 script_dir = Path(__file__).resolve().parent
 
-
+documents_path = os.path.join(script_dir, r"../../../data/raw/")
 dataset_path = os.path.join(script_dir, r"../../../data/processed/filtered.json")
 dataset_with_text_path = os.path.join(script_dir, r"../../../data/processed/filtered_with_text.json")
 
-
-def download_file(url):
-    """Downloads a file from the given URL and saves it to a temporary file."""
-    response = requests.get(url, stream=True)
-
-    # if response.status_code == 200:
-    #     ext = url.split('.')[-1].lower()
-    #     temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=f".{ext}")
-    #     temp_file.write(response.content)
-    #     temp_file.close()
-    #     return temp_file.name
-    # else:
-    #     raise Exception(f"Failed to download file: {response.status_code}")
-
+def extract_text(documentId):
+    try:
+        try:
+            path = os.path.join(documents_path, f"{documentId}.pdf")
+            document = fitz.open(path)
+            text = "\n".join([page.get_text() for page in document])
+            return text
+        except:
+            path = os.path.join(documents_path, f"{documentId}.doc")
+            doc = Document(path)
+            text = "\n".join([para.text for para in doc.paragraphs])
+            return text
+    except:
+        return ""
 
 with open(dataset_path, "r", encoding="utf-8") as fp:
     records = json.load(fp)
 
 # Iterate over documents
-for record in records:
+for record in tqdm(records):
     for document in record["documents"]:
         # Extract text
         url = document["url"]
-        download_file(url)
-        document["text"] = "test"
+        document["documentText"] = extract_text(document["id"])
     
-
 # Save dataset with texts
 with open(dataset_with_text_path, "w") as fp:
     json.dump(records, fp)
